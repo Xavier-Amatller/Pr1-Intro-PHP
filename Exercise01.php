052638c0cdfecf2e8c617b939bf3de824bb805ba
Xavier-Amatller <reyo2004@gmail.com>
2024-09-17 18:48:05 +0200

    final de la clase

--- Exercise01.php
+++ Exercise01.php
@@ -3,13 +3,13 @@
 
 $val1 = rand(1,5);
 $val2 = rand(1,5);
-echo $val1;
-echo $val2;
+echo "Valor 1: $val1<br>";
+echo "Valor 1: $val2<br>";
 
-echo "La suma de $val1 y $val2 es:". ($val1+$val2)."<br>";
-echo "La resta de $val1 y $val2 es:". ($val1-$val2)."<br>";
-echo "La multiplicacion de $val1 y $val2 es:". ($val1*$val2)."<br>";
-echo "La division de $val1 y $val2 es:". ($val1/$val2)."<br>";
+echo "<br>"."La suma de $val1 y $val2 es:". ($val1+$val2)."<br>";
+echo "<br>"."La resta de $val1 y $val2 es:". ($val1-$val2)."<br>";
+echo "<br>"."La multiplicacion de $val1 y $val2 es:". ($val1*$val2)."<br>";
+echo "<br>"."La division de $val1 y $val2 es:". ($val1/$val2)."<br>";
 
 ?>
 <html>
