81e8ed2d6265ae67dd57605b4c12cedf6890bbd4
Xavier-Amatller <reyo2004@gmail.com>
2024-09-17 18:27:59 +0200

    Crear archivo ejercicio1

---
+++ Exercise01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+
+$val1 = rand(1,5);
+$val2 = rand(1,5);
+echo $val1;
+echo $val2;
+
+echo "La suma de $val1 y $val2 es:". ($val1+$val2)."<br>";
+echo "La resta de $val1 y $val2 es:". ($val1-$val2)."<br>";
+echo "La multiplicacion de $val1 y $val2 es:". ($val1*$val2)."<br>";
+echo "La division de $val1 y $val2 es:". ($val1/$val2)."<br>";
+
+?>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+
+</body>
+</html>
