573552d1ebb693d9a302fc05dc57ba631f34d77f
Xavier-Amatller <reyo2004@gmail.com>
2024-09-18 20:01:22 +0200

    Inicio  y final Ej2

---
+++ Exercise02.php .php
@@ -0,0 +1,35 @@
+<?php
+echo"1. Se declara una variable con un valor numérico. UƟlizando una estructura switch/case
+genera un mensaje que indique a qué día de la semana se corresponde.
+a. En caso de que no sea un valor entre el 1 y el 7 indica que no se corresponde
+con ningún día.<br> ";
+
+$var = rand(1,10);
+echo "Valor -> $var <br>";
+switch ($var) {
+    case '1':
+        echo "Es lunes.";
+        break;
+        case '2':
+            echo "Es martes.";
+            break;
+                case '3':
+                    echo "Es miercoles.";
+                    break;
+                    case '4':
+                        echo "Es jueves.";
+                        break;
+                        case '5':
+                            echo "Es viernes.";
+                            break;
+                            case '6':
+                                echo "Es sabado.";
+                                break;
+                                case '7':
+                                    echo "Es domingo.";
+                                    break;
+    
+    default:
+        echo"No es un numero del 1 al 7";
+        break;
+}
