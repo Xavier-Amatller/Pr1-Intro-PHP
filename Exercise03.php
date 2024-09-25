967e51889cbd14fe61560623d44acb76208f1132
Xavier-Amatller <reyo2004@gmail.com>
2024-09-25 16:57:49 +0200

    Ej 3 acabado

--- Exercise03.php
+++ Exercise03.php
@@ -11,15 +11,23 @@ echo "Variable 2 -> $var2 <br>";
 echo "2. Muestra la progresión numérica de los números pares desde 0 hasta el valor de la
 primera con un bucle for.<br><br>";
 
-for ($i=0; $i < $var1; $i+2) { 
+for ($i=0; $i <= $var1; $i+=2) { 
     echo "$i<br>";
 }
 
 echo "3. Muestra la progresión numérica desde la segunda variable hasta 0 con un bucle
 while.<br><br>";
 
+
+for ($i=$var2; $i >= 0; $i--) { 
+    echo "$i<br>";
+}
 echo "4. Muestra la progresión numérica desde la primera variable a la segunda con un bucle
 do/while.<br>
 a. Si la segunda variable es más pequeña, sólo queremos que imprima una vez
 el valor de la primera variable. <br><br>";
-
+$aux = $var1;
+do{
+    echo "$aux<br>";
+    $aux++;
+}while($aux<=$var2);
