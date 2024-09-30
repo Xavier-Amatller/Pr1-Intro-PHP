45e7fb90d4cb301d655cc2968a850df0178e701c
Xavier-Amatller <reyo2004@gmail.com>
2024-09-30 18:25:44 +0200

    Creacion ej Sessions

---
+++ ExerciseSession01.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>Crea un formulario que permita gesƟonar la canƟdad de refresco o leche que hay en un
+supermercado.</h1><br><br>";
+
+echo "a) Se debe mantener el nombre del trabajador que está uƟlizando la aplicación.
+<br><br>";
+
+echo "b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
+
+<br><br>";
+echo "c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
+caso mostrar error.
+<br><br>";
